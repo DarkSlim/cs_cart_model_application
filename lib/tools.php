3543a0867ad3185c866dbff65180acd7825501cc
Zlatko Derkoski <zlatkoflashtest@gmail.com>
2013-06-06 12:22:09 +0200

    no message

--- lib/tools.php
+++ lib/tools.php
@@ -18,7 +18,7 @@ class Tools {
     }
     /////////////////////////////////////////////////////////////////////
     //Get total product count
-    public static function getTotalProductCount(){
+    public static function getTotalProductCount() {
         Db_Actions::DbSelect("SELECT COUNT(id) FROM  cscart_products");
     }
     /////////////////////////////////////////////////////////////////////
@@ -155,6 +155,66 @@ class Tools {
         return $product_data;
     }
     /////////////////////////////////////////////////////////////////////////
+    // Get total products count
+    public static function getTotalproductsCount($cat_id) {
+        //First get the products ids from the selected category
+        $products_ids = self::getCategoryProducts($cat_id, "");
+        $product_count = 0;
+        foreach ($products_ids as $product) {
+            $product_count++;
+        }
+        echo ceil($product_count / 9);
+    }
+    /////////////////////////////////////////////////////////////////////////
+    // Display products
+    public static function displayProductsData($category_id, $curr_page = 1) {
+        //First get the products ids from the selected category
+        $products_ids = self::getCategoryProducts($category_id, "", $curr_page);
+        $product_data = array();
+        foreach ($products_ids as $product) {
+            $product_data[] = array('product_id' => $product['product_id'],
+                'product_name' => self::getProductName($product['product_id']),
+                'product_image_url' => $root_url . self::getProductImage($product['product_id']),
+                'product_price' => self::getProductPrice($product['product_id']),
+                'category_id' => $product['category_ids']);
+        }
+
+        //display the products
+        $max_products_per_page = 9;
+        $offset = $curr_page * $max_products_per_page - $max_products_per_page;
+        $slice = array_slice($product_data, $offset, $max_products_per_page);
+        $counter = 0;
+        foreach ($slice as $product_item) {
+            $counter++;
+            if ($counter == 1) {
+                ?><div class="cs-product-row"><?php
+            }
+            ?>
+                <div class="cs-product" product_id="<?php echo $product_item['product_id'] ?>" product_price="<?php echo $product_item['product_price'] ?>" category_ids="<?php echo $product_item['category_id'] ?>">
+                    <img src="<?php echo $product_item['product_image_url'] ?>" width="97" height="126" alt="dress" class="cs-main-product-image" draggable="false" />
+                    <h3 class="cs-product-title"><?php echo substr($product_item['product_name'], 0, 14) ?></h3>
+                    <h4 class="cs-price"><?php echo $product_item['product_price'] ?></h4>
+                    <div class="cs-variations">
+                        <a href="#" class="cs-varr"><img src="img/product-images/variation-1.jpg" width="14" height="13" /></a>
+                        <a href="#" class="cs-varr"><img src="img/product-images/variation-2.jpg" width="14" height="13" /></a>
+                        <a href="#" class="cs-varr"><img src="img/product-images/variation-3.jpg" width="14" height="13" /></a>
+                    </div>
+                </div>
+                <?php
+                if ($counter == 3) {
+                    ?></div><?php
+                $counter = 0;
+            }
+        }
+    }
+    ////////////////////////////////////////////////////////////////////////
+    //Get total products count in category
+    public static function getTotalCategoryPageCount($cat_id) {
+
+        $category_data = self::getCategoryData($cat_id);
+        echo ceil((int) $category_data['product_count'] / 9);
+    }
+    /////////////////////////////////////////////////////////////////////////
     // Load background images
     public static function loadBackgroundImages() {
         $dir = scandir('../img/bgs');
@@ -173,7 +233,30 @@ class Tools {
         $max_bgs_per_page = 9;
         $offset = $curr_page * $max_bgs_per_page - $max_bgs_per_page;
         $slice = array_slice($bg_images, $offset, $max_bgs_per_page);
-        echo json_encode($slice);
+
+        $counter = 0;
+        $middleClass = "";
+        foreach ($slice as $bg) {
+            $counter++;
+            if ($counter == 1) {
+                ?><div class="cs-product-row"><?php
+                }
+                if ($counter == 2) {
+                    $middleClass = "cs-prd-middle";
+                }
+                else {
+                    $middleClass = "";
+                }
+                ?>
+                <div class="cs-product pad-bot <?php echo $middleClass; ?>">
+                    <img src="img/bgs/<?php echo $bg ?>" width="97" height="126" data-bgname="<?php echo $bg ?>" class="cs-main-bg" />
+                </div>
+                <?php
+                if ($counter == 3) {
+                    ?></div><?php
+                $counter = 0;
+            }
+        }
     }
     /////////////////////////////////////////////////////////////////////////
     // get number of pages with background images
@@ -301,4 +384,43 @@ if (isset($_POST['add_prd'])) {
     else {
         echo 2;
     }
+}
+/////////////////////////////////////////////////////////////////////////////////////////////////
+//Get products count
+if (isset($_POST['products_count'])) {
+    define('AREA', 'C');
+    require '../../prepare.php';
+    require '../../init.php';
+    require(DIR_ROOT . '/config.php');
+    require_once('../lib/db_actions.php');
+    require_once("../lib/tools.php");
+    $root_url = $config['current_location'];
+    $cat_id = (isset($_POST['cat_id']) && !empty($_POST['cat_id']) && is_numeric($_POST['cat_id'])) ? $_POST['cat_id'] : "";
+    Tools::getTotalproductsCount($cat_id);
+}
+/////////////////////////////////////////////////////////////////////////////////////////////////
+//Load products
+if (isset($_POST['load_products'])) {
+    define('AREA', 'C');
+    require '../../prepare.php';
+    require '../../init.php';
+    require(DIR_ROOT . '/config.php');
+    require_once('../lib/db_actions.php');
+    require_once("../lib/tools.php");
+    $root_url = $config['current_location'];
+    $cat_id = (isset($_POST['cat_id']) && !empty($_POST['cat_id']) && is_numeric($_POST['cat_id'])) ? $_POST['cat_id'] : "";
+
+    Tools::displayProductsData($cat_id, $_POST['page']);
+}
+//Get category products count
+if (isset($_POST['cat_products_count'])) {
+    define('AREA', 'C');
+    require '../../prepare.php';
+    require '../../init.php';
+    require(DIR_ROOT . '/config.php');
+    require_once('../lib/db_actions.php');
+    require_once("../lib/tools.php");
+    $root_url = $config['current_location'];
+
+    Tools::getTotalCategoryPageCount($_POST['catt_id']);
 }
