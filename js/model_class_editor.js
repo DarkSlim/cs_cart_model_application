07162370b115caacf6db1ca527e66da483e22be9
Zlatko Derkoski <zlatkoflashtest@gmail.com>
2013-06-06 16:40:59 +0200

    no message

--- js/model_class_editor.js
+++ js/model_class_editor.js
@@ -620,13 +620,13 @@ function CartItemModel()
     //ModelStage.MS.model.remove_item(ModelClothingPart.ALL_PARTS["__ovde doaga ajdito na produktot__"]);
     //ovde doaga ajdito na produktot, ova treba da bide ajdito na produktot
     this.add_to_bagg = function()
-    {   
+    {
     }
     /*GlobalEventor.GE.addEvent(GlobalEventor.ON_ADD_ITEM_TO_MODEL, function(item_added)
-    {
-        
-    });*/
-    
+     {
+     
+     });*/
+
     /*
      * 
      * @returns {undefined}
@@ -638,6 +638,41 @@ function CartItemModel()
     {
         //ja koristis ovaa niza.Taa terba da ima ceni, ajidja i site gluposti
         //ModelStage.MS.model.parts.length
+        if (ModelStage.MS.model.parts.length > 0) {
+            $('.cs-shopping-cart').show();
+        }
+        else {
+            $('.cs-shopping-cart').hide();
+        }
+        $('.cs-shopping-cart').find('.cs-selected-product').remove();
+        for (var i = 0; i < ModelStage.MS.model.parts.length; i++) {
+            //alert(ModelStage.MS.model.parts[i].price);
+            //alert(ModelStage.MS.model.parts[i].product_id);
+            var productCartHtml = '<div class="cs-selected-product" producId="' + ModelStage.MS.model.parts[i].product_id + '">';
+            productCartHtml += '<a href="#" class="cs-remove-product"></a>';
+            productCartHtml += '<p class="cs-prd-name">undefined</p>';
+            productCartHtml += '<p class="cs-prd-price"><strong>$' + ModelStage.MS.model.parts[i].price + '</strong></p>';
+            productCartHtml += '</div>';
+            $(productCartHtml).insertAfter('.cs-cart-items');
+        }
+        //update amount
+        ModelStage.MS.cart_item_model.updateTotalAmount();
+        //Remove product from cart
+        $('.cs-remove-product').each(function() {
+            $(this).on('click', function(event) {
+                var productID = $(this).parent().attr('producId');
+                ModelStage.MS.model.remove_item(ModelClothingPart.ALL_PARTS["__"+productID+"__"]);
+            })
+        })
+
+    }
+    //total price
+    this.updateTotalAmount = function() {
+        var totalAmount = 0;
+        for (var i = 0; i < ModelStage.MS.model.parts.length; i++) {
+            totalAmount += ModelStage.MS.model.parts[i].price;
+        }
+        $('.cs-cart-total').html("TOTAL: $" + totalAmount.toFixed(2));
     }
 }
 
