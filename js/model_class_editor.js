291933fc0df3bc4414aadb7f4dadf3b820431730
Zlatko Derkoski <zlatkoflash@gmail.com>
2013-06-06 16:19:50 +0200

    Zlatko

--- js/model_class_editor.js
+++ js/model_class_editor.js
@@ -107,19 +107,68 @@ GlobalEventor.GE = new GlobalEventor();
  */
 GlobalEventor.ON_START_LOADING = "ON_START_LOADING";
 GlobalEventor.ON_END_LOADING = "ON_END_LOADING";
+GlobalEventor.ON_START_SAVING_TEMPLATE = "ON_START_SAVING_TEMPLATE";
+GlobalEventor.ON_SAVING_TEMPLATE_COMPLETE = "ON_SAVING_TEMPLATE_COMPLETE";
+GlobalEventor.ON_START_TEMPLATE_OPEN = "ON_START_TEMPLATE_OPEN";
+GlobalEventor.ON_TEMPLATE_OPEN_COMPLETE = "ON_TEMPLATE_OPEN_COMPLETE";
 
 
 function TemplateModerator()
 {
+    this.template_is_opened = false;
+    this.opened_template_id = -1;
     this.save = function()
     {
-        $.post("");
+        GlobalEventor.GE.dispatch_event(GlobalEventor.ON_START_SAVING_TEMPLATE, {});
+        var object = {};
+        object.action = "create_and_save_template";
+        if(this.template_is_opened)
+        {
+            object.action = "save_template";
+            object.card_designer_templates_id = this.opened_template_id;
+        }
+        object.total_parts = ModelStage.MS.model.parts.length;
+        for(var i=0;i<ModelStage.MS.model.parts.length;i++)
+        {
+            object["price_"+i] = ModelStage.MS.model.parts[i].price;
+            object["product_id_"+i] = ModelStage.MS.model.parts[i].product_id;
+        }
+        $.post("lib/templates_moderator.php", object, function(data)
+        {
+            console.log("Saved template id:"+data);
+            ModelStage.MS.template_moderator.template_is_opened = true;
+            ModelStage.MS.template_moderator.opened_template_id = data;
+            GlobalEventor.GE.dispatch_event(GlobalEventor.ON_SAVING_TEMPLATE_COMPLETE, {});
+        });
     }
-    this.open = function()
+    this.open = function(card_designer_templates_id)
     {
-        
+        GlobalEventor.GE.dispatch_event(GlobalEventor.ON_START_TEMPLATE_OPEN, {});
+        var object = {};
+        object.action = "open_template";
+        object.card_designer_templates_id = card_designer_templates_id;
+        $.post("lib/templates_moderator.php", object, function(data)
+        {
+            console.log("Saved template id:"+data);
+            var xml_data = $.parseXML(data);
+            ModelStage.MS.template_moderator.template_is_opened = true;
+            ModelStage.MS.template_moderator.opened_template_id = $(xml_data).find("template_id").text();
+            for(var i=0;i<$(xml_data).find("part").length;i++)
+            {
+                var partXML = $(xml_data).find("part").get(i);
+                    ModelStage.MS.model.add_item(new ModelClothingPart(
+                            {
+                                product_id: $(partXML).find("product_id").text(),
+                                price: $(partXML).find("price").text()
+                            }));
+            }
+            GlobalEventor.GE.dispatch_event(GlobalEventor.ON_TEMPLATE_OPEN_COMPLETE, {});
+        });
     }
 }
+TemplateModerator.prototype = new TemplateModerator();
+TemplateModerator.AFTER_OPEN_TEMPLATE = "AFTER_OPEN_TEMPLATE";
+TemplateModerator.AFTER_SAVE_TEMPLATE = "AFTER_SAVE_TEMPLATE";
 
 function ImageModerator(image_details)
 { 
@@ -815,6 +864,11 @@ $(document).ready(function(e)
     $(".cs-save").click(function(e)
     {
         ModelStage.MS.template_moderator.save();
+        return false;
     });
+    
+    console.log("search into model_class_editor.js, this[ModelStage.MS.template_moderator.open(4);].");
+    console.log("it is function for opening project");
+    //ModelStage.MS.template_moderator.open(4);
     
 });
--- lib/templates_moderator.php
+++ lib/templates_moderator.php
@@ -6,4 +6,81 @@
  */
 require_once("db_actions.php");
 
+class TemplateModerator
+{
+    private  static function add_parts_into______card_designer_templates_parts($template_row_data)
+    {
+        for($i=0;$i<$_POST["total_parts"];$i++)
+        {
+            Db_Actions::DbInsert("
+                INSERT INTO card_designer_templates_parts(card_designer_templates_id,product_id,product_price)
+                VALUES(
+                '".$template_row_data->id."',
+                '".$_POST["product_id_".$i]."',
+                '".$_POST["price_".$i]."'
+                )
+            ");
+        }
+    }
+    /*
+     * When no template
+     */
+    public static function create_and_save_template()
+    {       
+        Db_Actions::DbInsert("
+                INSERT INTO card_designer_templates(user_id)
+                VALUES('-1')
+            ");
+        $lasttempalte = Db_Actions::DbSelectLastRow("card_designer_templates");
+        self::add_parts_into______card_designer_templates_parts($lasttempalte);
+        print $lasttempalte->id;
+    }
+    /*
+     * For opened template, when do reseving
+     */
+    public static function save_template()
+    { 
+        Db_Actions::DbDelete("
+            DELETE FROM card_designer_templates_parts 
+            WHERE card_designer_templates_id='".$_POST["card_designer_templates_id"]."'
+        ");
+        $template = Db_Actions::DbSelectRowByID("card_designer_templates", $_POST["card_designer_templates_id"]);
+        self::add_parts_into______card_designer_templates_parts($template);
+        print $_POST["card_designer_templates_id"];
+    }
+    public static function open_template()
+    {    
+        Db_Actions::DbSelect("
+            SELECT * FROM card_designer_templates_parts 
+            WHERE 
+            card_designer_templates_id='".$_POST["card_designer_templates_id"]."'
+        ");
+        $all_rows_products = Db_Actions::DbGetResults('array');
+        //print_r($all_rows_products);
+        $source_xml_data = "<source_template>";
+        $source_xml_data .= "<template_id>".$_POST["card_designer_templates_id"]."</template_id>";
+        $source_xml_data .= "<parts>";
+        for($i=0;$i<count($all_rows_products);$i++)
+        {
+            $source_xml_data .= "<part><product_id>".$all_rows_products[$i]["product_id"]."</product_id><price>".$all_rows_products[$i]["product_price"]."</price></part>";
+        }
+        $source_xml_data .= "</parts>";
+        $source_xml_data .= "</source_template>";
+        print $source_xml_data;
+    }
+}
+
+if($_POST["action"]=="create_and_save_template")
+{
+    TemplateModerator::create_and_save_template();
+}
+if($_POST["action"]=="save_template")
+{
+    TemplateModerator::save_template();
+}
+if($_POST["action"]=="open_template")
+{
+    TemplateModerator::open_template();
+}
+
 ?>
